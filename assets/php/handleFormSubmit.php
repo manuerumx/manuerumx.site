3f90d4c44a70282c2dd3663023c2426bf0390db3
Manuel González Rivera <mgrivera@gmail.com>
2015-04-17 13:04:05 -0500

    Added contact me function

--- assets/php/handleFormSubmit.php
+++ assets/php/handleFormSubmit.php
@@ -1,10 +1,24 @@
 <?php
-
-if(isset($_POST['c_name'])){
-    
-    $res['sendstatus'] = 1;
-    $res['message'] = 'Form Submission Succesful';
-    echo json_encode($res);
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+    empty($_POST['email']) 		||
+    empty($_POST['message'])	||
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+{
+    echo "No arguments Provided!";
+    return false;
 }
 
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+$to = 'mgrivera@gmail.com';
+$email_subject = "Formulario de contacto:  $name";
+$email_body = "Has recibido un nuevo correo desde el formulario de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
+$headers = "From: mgrivera@gmail.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
+//$headers = "From: ".$email_address."\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
 ?>
